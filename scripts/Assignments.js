fdc9c93aa68fbcd2d387a2b3a95831e2927b6070
Tingting Duan <duan.tingting@yahoo.com>
2021-05-04 21:30:50 -0500

    all bug fixed

--- scripts/Assignments.js
+++ scripts/Assignments.js
@@ -6,10 +6,11 @@ const walkers = getWalkers()
 
 
 // Function whose responsibility is to find the walker assigned to a pet
-const findWalker = (pet, allWalker) => {
+//const findWalker = (pet, allWalker) => {
+const findWalker = (pet, walkers) => {
     let petWalker = null
 
-    for (const walker of allWalkers) {
+    for (const walker of walkers) {
         if (walker.id === pet.walkerId) {
             petWalker = walker
         }
@@ -23,8 +24,8 @@ export const Assignments = () => {
     assignmentHTML = "<ul>"
 
     for (const currentPet of pets) {
-        const currentPetWalker = findPetWalker(currentPet, walkers)
-        assignmentHTML = `
+        const currentPetWalker = findWalker(currentPet, walkers)
+        assignmentHTML += `
             <li>
                 ${currentPet.name} is being walked by
                 ${currentPetWalker.name} in ${currentPetWalker.city}
